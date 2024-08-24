e6e54036f9e4bcccbeae353b6004630168900482
wrqqqr <wrqqqrrr@rambler.ru>
2024-08-24 18:22:21 +0300

    fixes

--- main.go
+++ main.go
@@ -43,7 +43,5 @@ func main() {
 	go cfg.crawlPage(rawBaseURL)
 	cfg.wg.Wait()
 
-	for normalizedURL, count := range cfg.pages {
-		fmt.Printf("%d - %s\n", count, normalizedURL)
-	}
+	printReport(cfg.pages, rawBaseURL)
 }
